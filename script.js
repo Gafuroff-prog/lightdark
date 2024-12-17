bff9639547e8a5112cb302db4ec88efc3d185f38
Gafuroff-prog <gafuroffgaming@gmail.com>
2024-12-17 13:12:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  }
+
+  // Kunduzi rejim funksiyasi
+  function lightMode() {
+    document.body.style.backgroundColor = "#C4B5A5";
+    document.body.style.color = "black";
+  }
